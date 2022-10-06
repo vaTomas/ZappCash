b24b3501ed033d61226004c667be5b3af9ff0b07
BuildTools <unconfigured@null.spigotmc.org>
2022-10-06 17:42:50 +0800

    Add project files.

---
+++ forms/frmDebugConsole.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ZappCash.forms
+{
+    partial class frmDebugConsole
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(22, 408);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(666, 20);
+            this.txtCommand.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(22, 13);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(755, 372);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(702, 408);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(819, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmDebugConsole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtCommand);
+            this.Name = "frmDebugConsole";
+            this.Text = "frmDebugConsole";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ forms/frmDebugConsole.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using ZappCash.packages;
+
+namespace ZappCash.forms
+{
+    public partial class frmDebugConsole : Form
+    {
+        public frmDebugConsole()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string content = txtOutput.Text;
+            IdGenerator hi = new IdGenerator();
+            string ParentID = txtCommand.Text;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vince\source\repos\ZappCash\data\accounts.mdf;Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Accounts values (@Id,@Name,@Description,@Code,@Color,@AssetType,@IsPlaceholder,@Parent,@SmallestFraction,@Balance)", con);
+            cmd.Parameters.AddWithValue("@Id", hi.GenerateID(ParentID));
+            cmd.Parameters.AddWithValue("@Name", "");
+            cmd.Parameters.AddWithValue("@Description", "");
+            cmd.Parameters.AddWithValue("@Code", "");
+            cmd.Parameters.AddWithValue("@Color", "#ffffff");
+            cmd.Parameters.AddWithValue("@AssetType", "PHP");
+            cmd.Parameters.AddWithValue("@IsPlaceholder", 0);
+            cmd.Parameters.AddWithValue("@Parent", ParentID);
+            cmd.Parameters.AddWithValue("@SmallestFraction", 2);
+            cmd.Parameters.AddWithValue("@Balance", 2);
+
+            cmd.ExecuteNonQuery();
+
+            
+
+            content += $"Successfully Saved {hi.GenerateID()} \r\n";
+
+            txtOutput.Text = content;
+        }
+    }
+}
